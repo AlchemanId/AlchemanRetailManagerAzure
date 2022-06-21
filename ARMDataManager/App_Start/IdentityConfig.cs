2ca2d154e7a47613d63ab8e5ccc339922a840064
Haikal Lazuardy <94656589+AlchemanId@users.noreply.github.com>
2022-06-21 16:34:00 +0700

    Update IdentityConfig.cs
    change to 5 length

--- ARMDataManager/App_Start/IdentityConfig.cs
+++ ARMDataManager/App_Start/IdentityConfig.cs
@@ -28,7 +28,7 @@ namespace ARMDataManager
             // Configure validation logic for passwords
             manager.PasswordValidator = new PasswordValidator
             {
-                RequiredLength = 3,
+                RequiredLength = 4,
                 RequireNonLetterOrDigit = false,
                 RequireDigit = false,
                 RequireLowercase = false,
