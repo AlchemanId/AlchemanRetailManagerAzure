5bf3c5801c8a426de0726eee17ce0b713fd7bcfd
haikal <93.haikal.lazuardy@gmail.com>
2022-06-29 10:48:00 +0700

    Wire Up the WPF Shopping Cart
    get Add to cart working
    add the functionality to add items to the car, get the subtotal, and update quantity.

--- ARMDesktopUI/ViewModels/SalesViewModel.cs
+++ ARMDesktopUI/ViewModels/SalesViewModel.cs
@@ -42,9 +42,22 @@ namespace ARMDesktopUI.ViewModels
             }
         }
 
-        private BindingList<ProductModel> _cart;
+        private ProductModel _selectedProduct;
 
-        public BindingList<ProductModel> Cart
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set 
+            { 
+                _selectedProduct = value; 
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set 
@@ -54,7 +67,7 @@ namespace ARMDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -63,6 +76,7 @@ namespace ARMDesktopUI.ViewModels
             { 
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
@@ -70,7 +84,12 @@ namespace ARMDesktopUI.ViewModels
         {
             get
             {
-                return "0.00"; 
+                decimal subTotal = 0;
+                foreach (CartItemModel item in Cart)
+                {
+                    subTotal += item.Product.RetailPrice *  item.QuantityInCart;
+                }
+                return subTotal.ToString("C"); 
             }
         }
         public string Tax
@@ -96,12 +115,37 @@ namespace ARMDesktopUI.ViewModels
                 bool output = false;
                 // Make sure something is selected
                 // make sure there is an item quantity
+
+                if(ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity)
+                {
+                    output = true;
+                }
                 return output;
             }
         }
         public void AddToCart()
         {
-
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+            if (existingItem != null)
+            {
+                existingItem.QuantityInCart += ItemQuantity;
+                // HACK - there should be a better way of refreshing the cart display
+                Cart.Remove(existingItem);
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+                Cart.Add(item);
+            }
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
+            NotifyOfPropertyChange(() => existingItem.DisplayText);
         }
         public bool CanRemoveFromCart
         {
@@ -115,6 +159,7 @@ namespace ARMDesktopUI.ViewModels
         public void RemoveFromCart()
         {
 
+            NotifyOfPropertyChange(() => SubTotal);
         }
         public bool CanCheckOut
         {
