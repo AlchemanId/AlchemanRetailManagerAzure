2e05dbe02093e1a8112942cbdbaede3ccd25965f
haikal <93.haikal.lazuardy@gmail.com>
2022-07-04 14:13:45 +0700

    SQL Transaction using Dapper video 24
    Add transactions to SQL Data Access and use them for the SalesData call.

--- ARMDataManager.Library/DataAccess/SaleData.cs
+++ ARMDataManager.Library/DataAccess/SaleData.cs
@@ -16,9 +16,9 @@ namespace ARMDataManager.Library.DataAccess
             // start filling in the sale detail models we will save to the database
             List<SaleDetailDBModel> details = new List<SaleDetailDBModel>();
             ProductData products = new ProductData();
-            var taxRate = ConfigHelper.GetTaxRate()/100;
+            var taxRate = ConfigHelper.GetTaxRate() / 100;
 
-            foreach(var item in saleInfo.SaleDetails)
+            foreach (var item in saleInfo.SaleDetails)
             {
                 var detail = new SaleDetailDBModel
                 {
@@ -28,7 +28,7 @@ namespace ARMDataManager.Library.DataAccess
 
                 // Get the information about this product
                 var productInfo = products.GetProductById(detail.ProductId);
-                if(productInfo == null)
+                if (productInfo == null)
                 {
                     throw new Exception($"The product Id of {detail.ProductId} could not be found in the database.");
                 }
@@ -44,29 +44,43 @@ namespace ARMDataManager.Library.DataAccess
             }
 
             // create the sale model
-            SaleDBModel sale = new SaleDBModel{
+            SaleDBModel sale = new SaleDBModel {
                 SubTotal = details.Sum(x => x.PurchasePrice),
                 Tax = details.Sum(x => x.Tax),
                 CashierId = cashierId
             };
             sale.Total = sale.SubTotal + sale.Tax;
 
-            // save the sale model
-            SqlDataAccess sql = new SqlDataAccess();
-            sql.SaveData<SaleDBModel>("dbo.spSaleInsert", sale, "ARMData");
-
-            // get the id from the sale model
-            sale.Id = sql.LoadData<int, dynamic>("dbo.spSaleLookup", new { sale.CashierId, sale.SaleDate }, "ARMData").FirstOrDefault();
-
-            // Finish filling in the sale detail models 
-            foreach(var item in details)
+            using (SqlDataAccess sql = new SqlDataAccess())
             {
-                item.SaleId = sale.Id;
-                
-                // save the sale detail models
-                sql.SaveData("dbo.spSaleDetailInsert", item, "ARMData");
+                try
+                {
+                    sql.StartTransaction("ARMData");
+
+                    // save the sale model
+                    sql.SaveDataInTransaction("dbo.spSaleInsert", sale);
+
+                    // get the id from the sale model
+                    sale.Id = sql.LoadDataInTransaction<int, dynamic>("dbo.spSaleLookup", new { sale.CashierId, sale.SaleDate }).FirstOrDefault();
+
+                    // Finish filling in the sale detail models 
+                    foreach (var item in details)
+                    {
+                        item.SaleId = sale.Id;
+
+                        // save the sale detail models
+                        sql.SaveDataInTransaction("dbo.spSaleDetailInsert", item);
+                    }
+                    //sql.CommitTransaction();
+                }
+                catch
+                {
+                    sql.RollbackTransaction();
+                    throw;
+                }
             }
 
+            
 
         }
     }
